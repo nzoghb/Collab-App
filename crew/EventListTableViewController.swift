060d6c0db21e1df9bf1dcbfaa0769d5a2c0022f5
Maaz Uddin <maaz.uddin@berkeley.edu>
2016-04-24 19:36:25 -0700

    Started the design portion

---
+++ crew/EventListTableViewController.swift
@@ -0,0 +1,24 @@
+//
+//  EventListTableViewController.swift
+//  crew
+//
+//  Created by Maaz Uddin on 4/22/16.
+//  Copyright © 2016 iOS Decal. All rights reserved.
+//
+
+import UIKit
+
+class EventListTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
---
+++ crew/crew/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  crew
+//
+//  Created by Maaz Uddin on 4/22/16.
+//  Copyright © 2016 iOS Decal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Open: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        Open.target = self.revealViewController()
+        Open.action = Selector("revealToggle:")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
