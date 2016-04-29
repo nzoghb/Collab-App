51787378b17c27ee4edc7ef5aa7b083abd8e6f81
Maaz Uddin <maaz.uddin@berkeley.edu>
2016-04-28 18:11:27 -0700

    Added colors and variables tto each of the tabbed views. Going to implement the sidebar menu right now

---
+++ crew/crew/CrewEventsView.swift
@@ -0,0 +1,30 @@
+//
+//  CrewView.swift
+//  crew
+//
+//  Created by Maaz Uddin on 4/27/16.
+//  Copyright © 2016 iOS Decal. All rights reserved.
+//
+
+import UIKit
+
+class CrewEventsView: UIViewController {
+
+    @IBOutlet weak var Menu: UIBarButtonItem!
+    @IBOutlet weak var NavigationBar: UINavigationBar!
+    @IBOutlet weak var TableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        Menu.target = self.revealViewController()
+        Menu.action = Selector("revealToggle:")
+        
+        NavigationBar.barStyle = UIBarStyle.Black
+        NavigationBar.tintColor = UIColor.whiteColor()
+        
+
+    }
+    
+}
---
+++ crew/crew/MyEventsView.swift
@@ -0,0 +1,28 @@
+//
+//  MyEventsView.swift
+//  crew
+//
+//  Created by Maaz Uddin on 4/27/16.
+//  Copyright © 2016 iOS Decal. All rights reserved.
+//
+
+import UIKit
+
+class MyEventsView: UIViewController {
+
+    @IBOutlet weak var Menu: UIBarButtonItem!
+    @IBOutlet weak var NavigationBar: UINavigationBar!
+    @IBOutlet weak var TableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        Menu.target = self.revealViewController()
+        Menu.action = Selector("revealToggle:")
+        
+        NavigationBar.barStyle = UIBarStyle.Black
+        NavigationBar.tintColor = UIColor.whiteColor()
+    }
+    
+}
