b54386e32205968b8481e0273fa16394e6c5ae70
Maaz Uddin <maaz.uddin@berkeley.edu>
2016-05-01 17:54:47 -0700

    Couldn't get the cocoapods for the calendar to work; scrapped it; wasted a lot of time -.- but going to work on the rest

---
+++ crew/crew/NewEventViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NewEventViewController.swift
+//  crew
+//
+//  Created by Maaz Uddin on 5/1/16.
+//  Copyright © 2016 iOS Decal. All rights reserved.
+//
+
+import Cocoa
+
+class NewEventViewController: NSObject {
+
+}
